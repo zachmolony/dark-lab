83468a822c4f4b0454392c7a337c306e33f2f161
zachmolony <31472522+zachmolony@users.noreply.github.com>
2019-03-26 21:27:58 +0000

    installed wpcli

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // middleware
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist/public'));
 
 app.listen(3000, function() {
     console.log("listenin on andre 3000")
