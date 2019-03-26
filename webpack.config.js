70becb392a5cecb4e67469e9edc17d866012e426
zachmolony <31472522+zachmolony@users.noreply.github.com>
2019-03-26 21:07:43 +0000

    implemented webpack

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+// basic config boilerplate
+module.exports = {
+    // private entry point
+    entry: './src/scripts.js',
+    output: {
+        filename: './public/js/build.js'
+    },
+    watch: true
+}
