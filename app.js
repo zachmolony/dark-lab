44c21ae0ece86319ffbeb775dc8e7920a8ac2995
zachmolony <31472522+zachmolony@users.noreply.github.com>
2019-03-26 20:42:23 +0000

    created email form and post req

--- app.js
+++ app.js
@@ -8,3 +8,7 @@ app.listen(3000, function() {
     console.log("listenin on andre 3000")
 });
 
+// when we get a req from the root (/) of the site, run middle code
+app.post('/', function(req, res) {
+    res.end('yee');
+})
