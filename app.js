4ec0ff09a948498a1eb2d9e237a5d33429ed33ff
zachmolony <31472522+zachmolony@users.noreply.github.com>
2019-03-27 10:24:40 +0000

    added bodyparser middleware to extract email from req
    req packet is url encoded but i added json as well for futre

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const bodyParser = require('body-parser');
 
 // middleware
 app.use(express.static(__dirname + '/dist/public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
+// parse application/json
+app.use(bodyParser.json())
+
+// create server
 app.listen(3000, function() {
     console.log("listenin on andre 3000")
 });
 
 // when we get a req from the root (/) of the site, run inside code
 app.post('/', function(req, res) {
+    console.log(req.body);
     res.end('post route success');
 })
--- src/scripts.js
+++ src/scripts.js
@@ -2,13 +2,14 @@ var $ = require('jquery');
 
 // prevent page reload on submit
 $('form').submit(function (event) {
+    var userEmail = $('#email').val();
     event.preventDefault();
     // send ajax req to server
     $.ajax({
         url: '/',
         type: 'POST',
         data: {
-            email: 'zach.molony@mail.com'
+            email: userEmail
         },
         success: function (response) {
             console.log(response);
